e3878bd8d3f3c45c0b71198cb0f867030caaa632
walid404 <walid.max28@gmail.com>
2022-11-28 16:36:07 +0200

    File Treate With Document

---
+++ Document.py
@@ -0,0 +1,20 @@
+import os
+
+documentsNames = []
+
+def ReadDocumentCollection(path):
+    documentsContent = []
+    global documentsNames
+    documentsNames = sorted(os.listdir(path))
+    for documentName in documentsNames:
+        documentsContent.append(ReadDocument(os.path.join(path, documentName)))
+
+    return documentsContent
+
+
+
+def ReadDocument(documentPath):
+    document = open(documentPath, 'r')
+    content = document.read()
+    document.close()
+    return content
