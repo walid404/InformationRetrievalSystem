2d5fa1c4c71d7392a8e90cd273bb9ac24ac4f496
walid404 <walid.max28@gmail.com>
2022-11-28 16:37:51 +0200

    File Treate With Tokenization

---
+++ Tokenizer.py
@@ -0,0 +1,31 @@
+import nltk.corpus
+from nltk.tokenize import word_tokenize
+
+
+def collectionTokenize( documentsContant):
+    collectionTokenized = []
+    for document in documentsContant:
+        collectionTokenized.append(word_tokenize(document))
+
+    return collectionTokenized
+
+
+
+def filterStopwords(documentTokens):
+    stopwords = getStopWords()
+    filteredDocument = []
+    for word in documentTokens:
+        if word not in stopwords:
+            filteredDocument.append(word)
+
+    return filteredDocument
+
+
+
+def getStopWords():
+    stopWord = set(nltk.corpus.stopwords.words('english'))
+    stopWord.remove('in')
+    stopWord.remove('to')
+    return  stopWord
+
+
